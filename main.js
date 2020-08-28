dcf146b95885331514d7d51c591f706a9606d42a
Michael Telatynski <7t3chguy@gmail.com>
2020-08-28 13:05:59 +0100

    Create environment to demonstrate UA iframe issue

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, session, BrowserWindow} = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -23,6 +23,8 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  session.defaultSession.setUserAgent("Custom UA");
+
   createWindow()
   
   app.on('activate', function () {
--- renderer.js
+++ renderer.js
@@ -4,3 +4,11 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    replaceText("ua", window.navigator.userAgent);
+})
